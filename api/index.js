bc91cc4ad28ad9488371f49e87bf9c07cbbc0211
ezParth <parthsingh55m@gmail.com>
2024-07-28 15:56:37 +0530

    adding cors for cross origin sharing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors")
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(cors());
+
+app.post("/register", (req, res) => {
+    const {username, password} = req.body;
+    res.json('Hello World!')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
